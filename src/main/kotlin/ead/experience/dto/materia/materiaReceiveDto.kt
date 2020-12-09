8fa3dd9bd0f24d9d2ed63e75a67eafdb0eb3eaca
lcuas coutinho <lucaspp1@ucl.br>
2020-12-08 22:11:36 -0300

    teste

--- src/main/kotlin/ead/experience/dto/materia/materiaReceiveDto.kt
+++ src/main/kotlin/ead/experience/dto/materia/materiaReceiveDto.kt
@@ -1,6 +1,19 @@
 package ead.experience.dto.materia
 
 import ead.experience.dto.materia.MateriaDto
+import java.io.File
 import javax.ws.rs.FormParam
 
-open class MateriaReceiveDto(@FormParam("id") var id: Int ) : MateriaDto()
+open class MateriaReceiveDto () {
+    @FormParam("id")
+    var id: Int = 0
+    @FormParam("nome")
+    var nome: String = ""
+    @FormParam("custo")
+    var custo: Float = 0F
+    @FormParam("idProfessor")
+    var idProfessor: Int = 0
+    @FormParam("foto")
+    var foto: File? = null
+
+}
