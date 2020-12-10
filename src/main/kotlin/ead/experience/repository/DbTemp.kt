388a3f607efe694b1a5970ffa30e772ea73132f8
lcuas coutinho <lucaspp1@ucl.br>
2020-12-09 21:41:03 -0300

    dlfg

--- src/main/kotlin/ead/experience/repository/DbTemp.kt
+++ src/main/kotlin/ead/experience/repository/DbTemp.kt
@@ -12,17 +12,17 @@ object DbTemp {
 
     val Professores =
             mutableListOf<Professor>(
-                    Professor(1, "pedro", "joão", "123", "pedro", null)
+                    /* Professor(1, "pedro", "joão", "123", "pedro", null) */
             )
 
     val Materias =
             mutableListOf<Materia>(
-                    Materia(1, "Português", 1200f, null, Professores[0])
+                    /* Materia(1, "Português", 1200f, null, Professores[0]) */
             )
 
     val AlunoMateria =
             mutableListOf<AlunoMateria>(
-                    AlunoMateria(1, Materias[0], Alunos[0], 0f)
+                    /* AlunoMateria(1, Materias[0], Alunos[0], 0f)*/
             )
 
     val Aulas =
